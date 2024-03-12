4992e58f26707f44930f164cb1f8884673116d8b
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-12 12:07:08 +0100

    Update GameWolfSheep.java

--- GameWolfSheep.java
+++ GameWolfSheep.java
@@ -5,52 +5,57 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random randNum = new Random();
-        char mis = 'M';
+        Mis mis = new Mis('M');
         Owca owca = new Owca('O');
         char[][] plansza = new char[6][6];
         int[] pozycjaOwcy = new int[8];
         int pozycjaMisiaX = 5;
-        int pozycjaMisiaY = randNum.nextInt(6);
-
-        for(int i=0; i < pozycjaOwcy.length; i++){
+        int pozycjaMisiaY = randNum.nextInt(plansza[0].length);
+        int pozycjaOwcyX = randNum.nextInt(plansza.length);
+        int pozycjaOwcyY = randNum.nextInt(plansza[0].length);
+        for (int i = 0; i < pozycjaOwcy.length; i++) {
             pozycjaOwcy[i] = randNum.nextInt(plansza.length);
         }
-        int x,y;
-        for(int i=0; i< 8; i++){
-            do{
-                x = pozycjaOwcy[i];
-                y = randNum.nextInt(plansza[0].length);
-            }while(plansza[x][y] == '0');
-            plansza[x][y] = 'O';
+        int x, y;
+        x = 0;
+        y = 0;
+        for (int i = 0; i < 8; i++) {
+            do {
+                pozycjaOwcyX = randNum.nextInt(plansza.length);
+                pozycjaOwcyY = randNum.nextInt(plansza[0].length);
+            } while (plansza[pozycjaOwcyX][pozycjaOwcyY] != 0);
+            plansza[pozycjaOwcyX][pozycjaOwcyY] = owca.znak;
         }
-
+        //druk planszy
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
-                if(plansza[i][j] != 'O' && plansza[i][j] != 'M'){
-                plansza[i][j] = '-';
+                if (plansza[i][j] != 'O' && plansza[i][j] != 'M') {
+                    plansza[i][j] = '-';
                 }
             }
         }
-        plansza[pozycjaMisiaX][pozycjaMisiaY] = mis;
+        plansza[pozycjaMisiaX][pozycjaMisiaY] = mis.znak;
+        plansza[pozycjaOwcyX][pozycjaOwcyY] = owca.znak;
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 System.out.print(plansza[i][j] + " ");
             }
             System.out.println();
         }
-        int numRand = randNum.nextInt(5)+1; // Corrected to generate a random number in the range [0, 5]
+        int numRand = randNum.nextInt(5) + 1; // Corrected to generate a random number in the range [0, 5]
 
         System.out.println("Wylosowana liczba to: " + numRand);
-        int k = 0; // Corrected to start from 0
+        int k = 0;
         while (k != numRand) {
             if (pozycjaMisiaX > 0) {
                 plansza[pozycjaMisiaX][pozycjaMisiaY] = '-';
                 pozycjaMisiaX--;
-                plansza[pozycjaMisiaX][pozycjaMisiaY] = mis;
+                plansza[pozycjaMisiaX][pozycjaMisiaY] = mis.znak;
                 k++;
                 System.out.println("Naciśnij spacje");
                 sc.nextLine();
             }
+            //Zrobic zeby zaliczalo ze mis zjadl owce
 
             for (int i = 0; i < 6; i++) {
                 for (int j = 0; j < 6; j++) {
@@ -60,20 +65,30 @@ public class Main {
             }
             System.out.println();
 
-            if (plansza[0][pozycjaMisiaY] == mis) {
-                System.out.println("Meta! Gratulacje!");
-                break;
-            }
+        if (plansza[0][pozycjaMisiaY] == mis.znak) {
+            System.out.println("Meta! Gratulacje!");
+            break;
+        }
         }
     }
 }
-class Zwierze{
-    int punkty;
-}
-class Owca extends Zwierze{
-    int pktZaZjedzenie;
-    char znak;
-    Owca(char znak){
-        this.znak = znak;
-    }
-}
+
+        class Zwierze {
+            int punkty;
+        }
+        class Owca extends Zwierze {
+            int pktZaZjedzenie;
+            char znak;
+            Owca(char znak) {
+                this.znak = znak;
+            }
+        }
+        class Mis extends Zwierze{
+        char znak;
+        Mis(char znak){
+            this.znak = znak;
+             }
+        }
+
+
+
