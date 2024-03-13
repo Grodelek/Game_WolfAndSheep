37e17fb110eb771aff51d5389b6f2aecbcd5cfb5
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-13 06:55:25 +0100

    Score board
    Bear now earn points for every sheep he eats

--- GameWolfSheep.java
+++ GameWolfSheep.java
@@ -7,46 +7,44 @@ public class Main {
         Random randNum = new Random();
         Mis mis = new Mis('M');
         Owca owca = new Owca('O');
-        char[][] plansza = new char[6][6];
-        int[] pozycjaOwcy = new int[8];
-        int pozycjaMisiaX = 5;
+        char[][] plansza = new char[15][6];
+        int[] tabOwca = new int[15];
+        int pozycjaMisiaX = plansza.length-1;
         int pozycjaMisiaY = randNum.nextInt(plansza[0].length);
-        int pozycjaOwcyX = randNum.nextInt(plansza.length);
-        int pozycjaOwcyY = randNum.nextInt(plansza[0].length);
-        for (int i = 0; i < pozycjaOwcy.length; i++) {
-            pozycjaOwcy[i] = randNum.nextInt(plansza.length);
+        int[] pozycjaOwcyX = new int[15];
+        int[] pozycjaOwcyY = new int[15];
+
+        mis.punkty=0;
+        for (int i = 0; i < tabOwca.length; i++) {
+            tabOwca[i] = randNum.nextInt(plansza.length);
         }
         int x, y;
         x = 0;
         y = 0;
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < tabOwca.length; i++) {
             do {
-                pozycjaOwcyX = randNum.nextInt(plansza.length);
-                pozycjaOwcyY = randNum.nextInt(plansza[0].length);
-            } while (plansza[pozycjaOwcyX][pozycjaOwcyY] != 0);
-            plansza[pozycjaOwcyX][pozycjaOwcyY] = owca.znak;
+                pozycjaOwcyX[i] = randNum.nextInt(plansza.length);
+                pozycjaOwcyY[i] = randNum.nextInt(plansza[0].length);
+            } while (plansza[pozycjaOwcyX[i]][pozycjaOwcyY[i]] != 0);
+            plansza[pozycjaOwcyX[i]][pozycjaOwcyY[i]] = owca.znak;
+
         }
-        //druk planszy
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < plansza.length; i++) {
+            for (int j = 0; j < plansza[i].length; j++) {
                 if (plansza[i][j] != 'O' && plansza[i][j] != 'M') {
                     plansza[i][j] = '-';
                 }
             }
         }
         plansza[pozycjaMisiaX][pozycjaMisiaY] = mis.znak;
-        plansza[pozycjaOwcyX][pozycjaOwcyY] = owca.znak;
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(plansza[i][j] + " ");
-            }
-            System.out.println();
-        }
-        int numRand = randNum.nextInt(5) + 1; // Corrected to generate a random number in the range [0, 5]
+        //Druk planszy
+        drukPlanszy(plansza);
+        
+        int rzutKostka = randNum.nextInt(5) + 1; // Corrected to generate a random number in the range [0, 5]
 
-        System.out.println("Wylosowana liczba to: " + numRand);
+        System.out.println("Wylosowana liczba to: " + rzutKostka);
         int k = 0;
-        while (k != numRand) {
+        while (k != rzutKostka) {
             if (pozycjaMisiaX > 0) {
                 plansza[pozycjaMisiaX][pozycjaMisiaY] = '-';
                 pozycjaMisiaX--;
@@ -55,40 +53,50 @@ public class Main {
                 System.out.println("Naciśnij spacje");
                 sc.nextLine();
             }
-            //Zrobic zeby zaliczalo ze mis zjadl owce
-
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 6; j++) {
+            for(int i=0; i<pozycjaOwcyX.length; i++){
+            if(plansza[pozycjaMisiaX][pozycjaMisiaY] == plansza[pozycjaOwcyX[i]][pozycjaOwcyY[i]]){
+                System.out.println("Mis zjadl owce");
+                 mis.punkty++;
+                }
+            }
+            for (int i = 0; i < plansza.length; i++) {
+                for (int j = 0; j < plansza[i].length; j++) {
                     System.out.print(plansza[i][j] + " ");
                 }
                 System.out.println();
             }
             System.out.println();
 
-        if (plansza[0][pozycjaMisiaY] == mis.znak) {
-            System.out.println("Meta! Gratulacje!");
-            break;
+            if (plansza[0][pozycjaMisiaY] == mis.znak) {
+                System.out.println("Meta! Gratulacje!");
+                System.out.println("Punktacja:\n Mis:"+mis.punkty+" Wilk: ");
+                break;
+            }
         }
+    }
+    static void drukPlanszy(char[][] plansza){
+        for (int i = 0; i < plansza.length; i++) {
+            for (int j = 0; j < plansza[i].length; j++) {
+                System.out.print(plansza[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
 
-        class Zwierze {
-            int punkty;
-        }
-        class Owca extends Zwierze {
-            int pktZaZjedzenie;
-            char znak;
-            Owca(char znak) {
-                this.znak = znak;
-            }
-        }
-        class Mis extends Zwierze{
-        char znak;
-        Mis(char znak){
-            this.znak = znak;
-             }
-        }
-
-
-
+class Zwierze {
+    int punkty;
+}
+class Owca extends Zwierze {
+    int pktZaZjedzenie;
+    char znak;
+    Owca(char znak) {
+        this.znak = znak;
+    }
+}
+class Mis extends Zwierze{
+    char znak;
+    Mis(char znak){
+        this.znak = znak;
+    }
+}
